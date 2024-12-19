045f4dc5e608fe87b08fd87efe4b9356a47bbc38
CopperCoder227 <526csteiner@frhsd.com>
2024-12-19 10:07:45 -0500

    image go brrrrrr

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//Random Number
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  let randomNumber = getRandomNumber(1, 55);
+  <span class="diff2">${randomNumber}</span>
+  
 //Generating prefix of name
 function genPrefix (firstName){
     if (firstName.length > 4){
